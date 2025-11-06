dd66ffb9b7b89be077fd7327cf1be2fb19f3e5b1
Aldrich <24028612@es.uveg.edu.mx>
2025-11-05 19:08:31 -0600

    Feat:Pagina de error modificada

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -20,7 +20,7 @@ export default function ErrorPage({ error, reset }: ErrorProps) {
         <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
           500
         </h1>
-        <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">
+        <p className="mb-4 text-3xl tracking-tight font-bold md:text-4xl">
           Algo salió mal.
         </p>
         <p className="mb-4 w-lg text-lg font-light text-gray-500 dark:text-gray-400">
