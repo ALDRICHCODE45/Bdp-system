87b7b4ceac66e434cd961716adb9c0c76cbf6a71
Aldrich <24028612@es.uveg.edu.mx>
2025-12-11 02:40:54 -0600

    UI:Mejorar el diseno del scanner QR

--- src/features/RecursosHumanos/asistencias/pages/QREntradasPage.tsx
+++ src/features/RecursosHumanos/asistencias/pages/QREntradasPage.tsx
@@ -1,33 +1,56 @@
 "use client";
+import Image from "next/image";
 import { QRCode } from "@/components/ui/shadcn-io/qr-code";
 import { Button } from "@/core/shared/ui/button";
-import { Card, CardContent, CardHeader } from "@/core/shared/ui/card";
 import { useRouter } from "next/navigation";
 
 const QREntradasPage = () => {
   const router = useRouter();
 
   return (
-    <>
-      <Card className="text-center">
-        <CardHeader>Escanea para registrar tu entrada o salida.</CardHeader>
-        <CardContent>
-          <section className="w-full max-w-2xl mx-auto flex justify-center items-center border border-gray rounded-lg">
+    <div className=" flex flex-col items-center justify-center px-4">
+      <div className="w-full max-w-lg space-y-8">
+        {/* Logo */}
+        <div className="flex justify-center">
+          <Image
+            src="/signIn/logo.webp"
+            alt="Logo BDP"
+            width={300}
+            height={300}
+            className="dark:invert dark:brightness-200"
+            priority
+          />
+        </div>
+
+        {/* Título */}
+        <div className="text-center space-y-2">
+          <h1 className="text-4xl font-semibold tracking-tight">
+            Registro de Asistencia
+          </h1>
+          <p className="text-lg text-muted-foreground">
+            Escanea el código QR para registrar tu entrada o salida
+          </p>
+        </div>
+
+        {/* QR Code */}
+        <div className="flex justify-center ">
+          <div className="p-4 bg-white rounded-xl shadow-sm w-sm">
             <QRCode data="http://192.168.100.14:3000/register-qr-entry" />
-          </section>
-          <div className="mt-5">
-            <Button
-              onClick={() => {
-                router.push("/register-qr-entry");
-              }}
-              variant={"default"}
-            >
-              Registrar manualmente
-            </Button>
           </div>
-        </CardContent>
-      </Card>
-    </>
+        </div>
+
+        {/* Botón */}
+        <div className="flex justify-center">
+          <Button
+            onClick={() => router.push("/register-qr-entry")}
+            variant="outline"
+            className="w-full max-w-sm text-sm"
+          >
+            Registrar manualmente
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 };
 
