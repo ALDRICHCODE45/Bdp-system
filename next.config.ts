187dab1fda6f33d8aef8c26da824615a5e86ed27
Aldrich <nocheblanca92@gmail.com>
2026-01-01 18:19:39 -0600

    Fixed(middleware) body size limit fixed

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     serverActions: {
-      bodySizeLimit: "10mb", // Permite archivos de hasta 10MB
+      bodySizeLimit: "1mb", // Permite archivos de hasta 10MB
     },
   },
 };
